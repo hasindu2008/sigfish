a9e5219dc47d8a75aeb33be186e1fb3f1bd59b00
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2022-04-15 01:08:42 +1000

    dtw single script

---
+++ scripts/dtw_single.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ] || [ -z "$2"]; then
+    echo "Usage: $0 ref_id read_id"
+    exit 1
+fi
+
+samtools faidx test/rnasequin_sequences_2.4.fa $1 > a.fa
+./sigfish dtw -g a.fa -s test/sequin_reads.blow5 -t 16 --rna --full-ref -q 500 -p -1 > a.paf
+grep $2 a.paf
+
+
+
