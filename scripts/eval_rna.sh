4ac834f280105b4a044f35b37a07a4578af45b9b
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2022-08-26 00:15:13 +1000

    cleanup a bit

--- scripts/eval_rna.sh
+++ scripts/eval_rna.sh
@@ -4,10 +4,10 @@ set -e
 
 HARU_VENV=~/haru/
 REF=test/rnasequin_sequences_2.4.fa
-BLOW5=test/sequin_reads.blow5
+BLOW5=test/rna_sequin/sequin_reads.blow5
 THREADS=8
-REF_PAF=test/rna.minimap2.paf
-MY_PAF=test/test.paf
+REF_PAF=test/rna_sequin/rna.minimap2.paf
+MY_PAF=test/rna_sequin/test.paf
 
 make
 #./sigfish dtw ${REF} ${BLOW5} -t ${THREADS} --rna --from-end -q 500 > ${MY_PAF}
@@ -17,5 +17,5 @@ make
 #./sigfish dtw ${REF} ${BLOW5} -t ${THREADS} --rna --full-ref -q 500 -p -1 > ${MY_PAF}
 
 source ${HARU_VENV}/bin/activate
-uncalled pafstats -r ${REF_PAF} ${MY_PAF} -a > err.paf
+uncalled pafstats -r ${REF_PAF} ${MY_PAF} -a > test/rna_sequin/err.paf
 ./sigfish eval ${REF_PAF} ${MY_PAF}
--- scripts/eval_rna_invert.sh
+++ scripts/eval_rna_invert.sh
@@ -4,8 +4,8 @@ set -e
 
 
 make
-./sigfish dtw test/rnasequin_sequences_2.4.fa test/sequin_reads.blow5 --rna --full-ref \
-    --invert -q100000 -p0 -K10 --debug-break=yes > test/res_rna.paf
-./sigfish dtw test/rnasequin_sequences_2.4.fa test/sequin_reads.blow5 --rna --full-ref \
-             -q100000 -p0 -K10 --debug-break=yes> test/res_rna2.paf
-diff test/res_rna.paf test/res_rna2.paf
+./sigfish dtw test/rnasequin_sequences_2.4.fa test/rna_sequin/sequin_reads.blow5 --rna --full-ref \
+    --invert -q100000 -p0 -K10 --debug-break=yes > test/rna_sequin/res_rna.paf
+./sigfish dtw test/rnasequin_sequences_2.4.fa test/rna_sequin/sequin_reads.blow5 --rna --full-ref \
+             -q100000 -p0 -K10 --debug-break=yes> test/rna_sequin/res_rna2.paf
+diff test/rna_sequin/res_rna.paf test/rna_sequin/res_rna2.paf
