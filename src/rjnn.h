a84ed9c0ef287248f25706e1e7fef6f1def863e2
Bonson Wong <boson.ym@gmail.com>
2024-03-18 13:16:44 +1100

    new ran004 params

--- src/rjnn.h
+++ src/rjnn.h
@@ -31,14 +31,14 @@ typedef struct {
 } \
 
 #define JNNV3_RNA004_ADAPTOR { \
-    .std_scale = 0.63, \
+    .std_scale = 0.7, \
     .corrector = 1200, \
     .seg_dist = 1800, \
-    .window = 400, \
+    .window = 300, \
     .error = 5, \
-    .min_seg_len = 4800, \
-    .chunk_size = 1200, \
-    .start_chunks = 11, \
+    .min_seg_len = 4000, \
+    .chunk_size = 2400, \
+    .start_chunks = 6, \
 } \
 
 typedef struct jnnv3_astate_s {
